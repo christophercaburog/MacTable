73a0c76016831703fe94fe3d8eaf7c96aab7bc67
Klong-nu <klong@klong.nu>
2013-01-23 03:50:24 +0100

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ MacTable
 ========
 
 Table for map addresses combined with names
+
+This software uses arpwatch, apache with php and mongodb to work.
+Just install them without changing any options and it will probably work.
+
+arptable.php should be run sometimes though a cronjob or alike
+
+setname.php is used to set names of devices
+
+devices.php is placed in web directory to list devices
---
+++ arptable.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+$a=file("/var/lib/arpwatch/arp.dat");
+$c=array();
+foreach ($a as $b) {
+$d=explode("\t",$b);
+if (!empty($d)) {
+if (!(empty($d[0]) and empty($d[1]))) {
+$e=array();
+$e["m"]=$d[0];
+$e["i"]=$d[1];
+$e["s"]=$d[2];
+if (!empty($d[3])) {
+$e["n"]=$d[3];
+}
+$c[]=$e;
+}
+}
+}
+$mon=new Mongo();
+$db=$mon->test;
+$col=$db->devices;
+foreach ($c as $f) {
+$r=$col->findOne(array("m"=>$f["m"]));
+if ($r!=null) {
+$col->update(array("_id"=>$r["_id"]),array('$set'=>$f));
+} else {
+$col->insert($f);
+}
+}
+echo "Done\n";
