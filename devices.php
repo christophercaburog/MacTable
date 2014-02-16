1411def1d9ed5eb53c64385bb4a9d6842b10ade6
Klong-nu <klong@klong.nu>
2014-02-16 14:59:04 +0100

    Update devices.php

--- devices.php
+++ devices.php
@@ -4,29 +4,29 @@ $asjson=filter_input(INPUT_GET,"asjson",FILTER_VALIDATE_BOOLEAN);
 $m=new Mongo();
 $a=$m->test->devices->find()->sort(array("i"=>1));
 if ($asjson) {
-$c=array();
-foreach ($a as $b) {
-unset($b["_id"]);
-if ($showhidden or isset($b["n"])) {
-$c[]=$b;
-}
-}
+ $c=array();
+ foreach ($a as $b) {
+  unset($b["_id"]);
+  if ($showhidden or isset($b["n"])) {
+   $c[]=$b;
+  }
+ }
 echo json_encode(array("devices"=>$c));
 } else {
 ?><table border="1">
 <tr><th>Name</th><th>Last seen ip</th><th>Mac</th><th>Last seen</th></tr>
 <?php
-foreach ($a as $b) {
-if (isset($b["i"])) {
-if (isset($b["n"])){
-echo "<tr><td>".$b["n"]."</td><td>".$b["i"]."</td><td>".$b["m"]."</td><td>".date(DATE_RSS,$b["s"])."</td></tr>\n";
-} else {
-if ($showhidden) {
-echo "<tr><td></td><td>".$b["i"]."</td><td>".$b["m"]."</td><td>".date(DATE_RSS,$b["s"])."</td></tr>\n";
-}
-}
-}
-}
+ foreach ($a as $b) {
+  if (isset($b["i"])) {
+   if (isset($b["n"])) {
+     echo "<tr><td>".$b["n"]."</td><td>".$b["i"]."</td><td>".$b["m"]."</td><td>".date(DATE_RSS,$b["s"])."</td></tr>\n";
+   } else {
+     if ($showhidden) {
+      echo "<tr><td></td><td>".$b["i"]."</td><td>".$b["m"]."</td><td>".date(DATE_RSS,$b["s"])."</td></tr>\n";
+     }
+    }
+   }
+  }
 ?>
 </table>
 <?php if ($showhidden) { ?>
