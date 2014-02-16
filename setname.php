487be3eb418a5e7f74ab09cefaddb144fb1c2df1
Klong-nu <klong@klong.nu>
2014-02-16 14:54:32 +0100

    Update setname.php

--- setname.php
+++ setname.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php
 if ($argc==3) {
-$mon=new Mongo();
-$db=$mon->test;
-$col=$db->devices;
-print_r($argv);
-$col->update(array("m"=>$argv[1]),array('$set'=>array("n"=>$argv[2])));
+ $mon=new Mongo();
+ $db=$mon->test;
+ $col=$db->devices;
+ print_r($argv);
+ $col->update(array("m"=>$argv[1]),array('$set'=>array("n"=>$argv[2])));
 }
